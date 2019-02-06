07d333b62fc49c1224f96e5c631b688f13becfc4
zcc <zhangchangcheng@chengantech.com>
2019-02-06 11:12:10 +0800

    Initial commit

---
+++ module01/src/cheng/bean/customer.java
@@ -0,0 +1,17 @@
+package cheng.bean;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+//这是单行注释
+public class customer {
+    public static void main(String[] args) {
+        System.out.println("hello world!!!!!");
+
+        ArrayList list = new ArrayList();
+
+        new Date();
+
+    }
+
+}
---
+++ src/cheng/template.java
@@ -0,0 +1,30 @@
+package cheng;
+
+public class template {
+//    模板1：psvm
+    public static void main(String[] args) {
+//        模板2：输出sout,  soutp, soutm,  soutv,  xxx.sout
+        System.out.println();
+
+        int num1 = 10;
+        System.out.println(num1);
+        System.out.println("num1 = " + num1);
+        System.out.println("template.main");
+        System.out.println("num1 = " + num1);
+//        模板3  fori
+
+        String[] arr = new String[]{"tom", "jerry", "hanmeimei"};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+
+//        变形：iter和itar
+//        变形：list.fori
+//        变形：list.forr
+
+//        模板5：ifn
+//        模板6：inn
+
+
+    }
+}
